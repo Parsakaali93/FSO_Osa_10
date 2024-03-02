bb4c4fbe58902f114b456df0bd31c038ebe74132
Parsakaali <a@a.com>
2024-03-02 14:16:16 +0200

    exercise 10.10 done

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,4 +1,5 @@
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import Text from './Text';
 
 const styles = StyleSheet.create({
   tinyLogo: {
