97b64451d58a3ef6938ecee8e2b82b4b8210ba15
Parsakaali <a@a.com>
2024-03-03 16:42:02 +0200

    10.11 done

--- App.js
+++ App.js
@@ -3,7 +3,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient';
 
+const apolloClient = createApolloClient();
 // const HelloWorld = () => {
 //   return <Text>Hello world!</Text>;};
 
@@ -21,7 +24,9 @@ export default function App() {
   return (
     <> 
       <NativeRouter>
+      <ApolloProvider client={apolloClient}>
             <Main />
+      </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" /> 
     </>
