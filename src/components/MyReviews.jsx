6ef217577afde2524ea8dabcfd9d0bedf11766eb
Parsakaali <a@a.com>
2024-08-18 23:08:15 +0300

    updated my reviews view for cases with no reviews or user signing out on the page

--- src/components/MyReviews.jsx
+++ src/components/MyReviews.jsx
@@ -10,15 +10,22 @@ const MyReviews = () => {
     if (loading) return <Text>Loading...</Text>;
     if (error) return <Text>Error fetching repositories: {error.message}</Text>;
 
+    if(!data.me)
+      return <Text>Please sign in to view your reviews</Text>
+
     const reviews = data.me.reviews.edges;
     
     return (
       <>
-        <Text>rsfsdf</Text>
-        <FlatList
+      { reviews.length > 0 ?         
+      <FlatList
           data={reviews}
           renderItem={({ item }) => <ReviewItem review={item.node} showRepoName={true}/>}
         />
+        :
+        <Text>You don't have any reviews</Text>
+
+      }
      </>
     );
     
